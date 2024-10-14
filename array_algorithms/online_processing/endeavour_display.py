7f3744aa64a3b9599b9c4a6f9319df3ced936a45
nharrison02 <161665836+nharrison02@users.noreply.github.com>
2024-10-14 15:29:18 +0100

    Monday

--- array_algorithms/online_processing/endeavour_display.py
+++ array_algorithms/online_processing/endeavour_display.py
@@ -26,6 +26,9 @@ eig_thresh = 100000
 angle_jump_thresh = 15
 smoothing_factor = 0.9
 scale_factor = 1.7
+n_sub = 5
+sep_sub = 0.24
+bf_plot_freq = 700
 
 # initialise empty arrays and algorithms
 ks = np.zeros(shape=(channels, init_blocks))
@@ -36,28 +39,55 @@ freq_range = [int(np.round(f / Fs * nfft)) for f in freq_range]
 freq_bins = np.arange(freq_range[0], freq_range[1], dtype=np.int64)
 
 mic_positions = np.array([[0.48,0.24,0.16,0.08,0,-0.08,-0.24,-0.48],[0]*channels])
+subarray_positions = pra.linear_2D_array(center=array_loc, M=n_sub, phi=0, d=sep_sub)
+beamformer = pra.Beamformer(subarray_positions, Fs, nfft)
 music = pra.doa.normmusic.NormMUSIC(mic_positions, Fs, nfft,num_src=num_source,azimuth = candidate_azs*np.pi/180,mode='near')
 music.freq_bins = freq_bins
 q = queue.Queue()
 
 grid_plotdata = np.zeros(len(candidate_azs))
 az_plotdata = np.zeros(num_source,)
+beam_plotdata = np.zeros([180,2])
 previous_az = np.zeros(num_source)
 
 # make fig and axis of matplotlib plt
-fig = plt.figure(figsize = (18,8))
-gs = fig.add_gridspec(1,2,wspace=0.1,hspace=0.1,)
+fig, hiddenax = plt.subplots(figsize = (18,8))
+hiddenax.axis('off')
+
+gs = fig.add_gridspec(2,2,left=0.05,right=0.95,wspace=0.1,hspace=0.1,height_ratios=(2,1),)
 ax1 = fig.add_subplot(gs[0,0],projection = 'polar')
-ax2 = fig.add_subplot(gs[0,1])
-ax = [ax1,ax2]
+ax2 = fig.add_subplot(gs[1,:])
+ax3 = fig.add_subplot(gs[0,1],projection='polar')
+ax = [ax1,ax2,ax3]
+
+# plot formatting
 ax[0].set_title("Estimated DOA")
-ax[1].set_title("NormMusic Grid")
+ax[0].grid(True)
+ax[0].set_ylim(0, 1)
+ax[0].set_xlim(0, np.pi)
+ax[0].xaxis.set_label_coords(0.9,1)
+ax[0].set_yticks([])
+ax[0].set_theta_direction(-1)
+ax[0].set_theta_offset(np.pi)
+ax[0].set_clip_on(False)
+
+ax[1].set_title("Subspace spectrum")
+ax[1].set_ylim(0,1)
+
+ax[2].set_title('Array Beam Pattern')
+ax[2].set_xlim(0, np.pi)
+ax[2].set_ylim(0,1)
+ax[2].set_theta_direction(-1)
+ax[2].set_theta_offset(np.pi)
+ax[2].set_yticks([])
 
 lines = []
 for i in range(num_source):
 	lines.append(ax[0].plot(az_plotdata[i]))
 
 lines.append(ax[1].plot(grid_plotdata))
+for i in range(2):
+    lines.append(ax[2].plot(np.arange(0,180,1)*np.pi/180,beam_plotdata[:,i]))
 
 # use an audio call back function to process data
 def audio_callback(indata,frames,time,status):
@@ -104,10 +134,10 @@ def audio_callback(indata,frames,time,status):
 	# matching game to assign azimuths closest to their last known value
 	az_vector = np.zeros(num_source)
 	grid_vector = np.zeros(candidate_azs.shape)
-
+	response_data = np.zeros([180, 2])
 	if music.num_src > 0:
 		music.locate_sources(X,freq_bins=freq_bins)
-		az = music.azimuth_recon*180/np.pi
+		az = (music.azimuth_recon*180/np.pi - 90) * scale_factor + 90
 		az.sort()
 		
 		distances = 360*np.ones((len(previous_az),len(az)))
@@ -132,37 +162,59 @@ def audio_callback(indata,frames,time,status):
 				az_vector[prev_i] = smoothing_factor*previous_az[prev_i] + (1 - smoothing_factor)*az[curr_i]
 		
 		grid_vector = music.grid.values
+		
+        # generate beam patterns for a given azimuth
+		if count_src > 2:
+			count_src = 2
+		detected_azs = az_vector[:count_src]
+		
+		source_locs = np.zeros(shape=(2,len(detected_azs)))
+		source_locs[0,:] = np.cos(detected_azs*np.pi/180)
+		source_locs[1,:] = np.sin(detected_azs*np.pi/180)
+		
+		for i in range(len(detected_azs)):
+			current_interf = source_locs
+			current_interf = np.delete(current_interf, i, 1)
+			interf = pra.soundsource.SoundSource([0,0])
+			interf.images = current_interf
+			source = pra.soundsource.SoundSource([0,0])
+			source.images = np.array([source_locs[0, i], source_locs[1, i]])# beamformer pattern at first azimuth
+			
+			beamformer.frequencies = np.array([bf_plot_freq])
+			beamformer.rake_max_sinr_weights(
+				source = source,
+				interferer = interf,
+				R_n= 1e-6*np.identity(5)
+				)
+			response = beamformer.response(np.arange(0,180,1)*np.pi/180, bf_plot_freq)
+			response_data[:,i] = np.abs(response[1])
+			
 	previous_az = az_vector
-	return_data = [az_vector, grid_vector]
+	return_data = [az_vector, grid_vector,response_data]
 	q.put(return_data)
 	
 # take frame of audio samples from the queue and update plot
 def update_plot(frame):
-	global az_plotdata, grid_plotdata
+	global az_plotdata, grid_plotdata,beam_plotdata
 	while True:
 		try: 
 			data = q.get_nowait()
 		except queue.Empty:
 			break
-		az_plotdata = (data[0] - 90)*scale_factor + 90
+		az_plotdata = data[0]
 		grid_plotdata = data[1]
+		beam_plotdata = data[2]
 	for i in range(len(az_plotdata)):
 		lines[i][0].set_ydata([0, 1])
 		lines[i][0].set_xdata([0, az_plotdata[i]*np.pi/180])
 
 	lines[num_source][0].set_xdata(candidate_azs)
 	lines[num_source][0].set_ydata(grid_plotdata)
-	ax[0].set_xlabel(f'Sources detected: {music.num_src}')
+	lines[num_source+1][0].set_ydata(np.abs(beam_plotdata[:,0])/np.max(np.abs(beam_plotdata[:,0])))
+	lines[num_source+2][0].set_ydata(np.abs(beam_plotdata[:,1])/np.max(np.abs(beam_plotdata[:,1])))
+	hiddenax.set_title(f'LIVE ARRAY PROCESSING \n Sources detected: {music.num_src}')
 	return lines
 
-# plot formatting
-ax[0].grid(True)
-ax[0].set_ylim(0, 1)
-ax[0].set_xlim(0, np.pi)
-ax[0].set_ylabel('Angle (Degrees)')
-ax[0].set_yticks([])
-
-ax[1].set_ylim(0,1)
 # outer_axs[1].set_yticks([])
 # outer_axs[1].set_xticks([])
 
